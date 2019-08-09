a794817bebab245389c0f1a340c0f99f3576aae2
caujoeng <carlylezqy@qq.com>
2019-08-09 18:51:15 +0900

    Add

--- CS101/Week_0/2-栈排序.cpp
+++ CS101/Week_0/2-栈排序.cpp
@@ -12,10 +12,13 @@ int main() {
         cin >> tmp;
         myStack.push(tmp);
     }
+    
     stack<int> result = sorting(myStack);
+    
     vector<int> answer;
     while (!result.empty()) {
-        answer.push_back(result.top());
+        answer.insert(answer.begin(),result.top()); 
+        //answer.push_back(result.top());
         result.pop();
     }
     for (auto i = answer.rbegin(); i != answer.rend(); ++i)
@@ -26,5 +29,23 @@ int main() {
 // myStack：输入栈，栈中的所有元素即是待排序的元素
 // 返回值：输出栈，即排序后的序列，满足从栈底至栈顶为升序
 stack<int> sorting(stack<int> myStack) {
-    /* 请在这里设计你的算法！ */
+    stack<int> ordered;
+    int tmp = 0;
+    int size = myStack.size();
+    ordered.push(myStack.top());
+    myStack.pop();
+    while(!myStack.empty() && ordered.size() < size){
+        tmp = myStack.top();
+        myStack.pop();
+        if(ordered.empty() || tmp <= ordered.top()){
+            ordered.push(tmp);
+        }else{
+            while(!ordered.empty() && tmp > ordered.top()){
+                myStack.push(ordered.top());
+                ordered.pop();
+            }
+            ordered.push(tmp);
+        }
+    }
+    return ordered;
 }
