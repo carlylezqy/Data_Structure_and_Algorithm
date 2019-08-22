4f8d0ae49b558407610c9d4521762d678d86c7c2
CauJoeng <carlylezqy@qq.com>
2019-08-22 21:57:44 +0900

    Update

--- CS101/Week_1/7.15 计算思维/任务1.2.cpp
+++ CS101/Week_1/7.15 计算思维/任务1.2.cpp
@@ -12,29 +12,28 @@ const int TARGET_H = 5;
 int path[TARGET_H] = {0}; // 方案细节
 int h = 0;
 
-void Downstairs_A(int i,int start);
+void Downstairs_A(int height,int step);
 void Downstairs_B(int height, int step);
-void Downstairs_rewrite(int height, int span);
 
-void Downstairs_A(int i,int start){
+void Downstairs_A(int height,int step){
     // i: i级台阶 j: 即将下降的台阶数
-    // start: 从第start步开始
+    // step: 从第step步开始
     for(int j=3; j>0; j--){
         if(i >= j){
-            take[start] = j;
+            take[step] = j;
             if(i == j){ // 到楼下
                 num++;
                 cout << "Case " << num << ":";
-                for (int k=1; k<=start; k++) cout << take[k] << ' ';
+                for (int k=1; k<=step; k++) cout << take[k] << ' ';
                 cout << endl;
 
-            } else Downstairs_A(i-j, start+1);
+            } else Downstairs_A(i-j, step+1);
         }
     }
 }
 void Downstairs_B(int height, int step){
-// 先判断中止，再枚举递归
-// 第step步，从高度height开始，继续下楼
+    // 先判断中止，再枚举递归
+    // 第step步，从高度height开始，继续下楼
     if (height == 0){ // 到楼下
         num++;
         cout << "Case " << num << ":";
@@ -50,41 +49,18 @@ void Downstairs_B(int height, int step){
         Downstairs_B(new_height, step+1); // 继续下降
     }
 }
-void Downstairs_rewrite(int height, int span){
-    if (height == 0){ // 到楼下
-        num++;
-        cout << "Case " << num << ":";
-        for(int i=1; path[i] != '\0'; i++) cout << path[i] << ' ';
-        cout << endl;
-        return;
-    }
-
-    int new_height = height - span; // 新高度
-    if(new_height < 0) return; // 是否合法
-    cout << span << ' ';
-
-    Downstairs_rewrite(new_height,1);
-    Downstairs_rewrite(new_height,2);
-    Downstairs_rewrite(new_height,3);// 继续下降
-}
 
 int main(){
     cout << "台阶数:";
     cin >> h;
 
-    //cout << "[Method_A]" << endl;
-    //Downstairs_A(h, 1); // 从第h级，开始下第一步
-
-    //num = 0; // 方案数初始化
-
-    //cout << "[Method_B]" << endl;
-    //Downstairs_B(TARGET_H, 0); // 第0步，从高度 TARGET_H 出发
+    cout << "[Method_A]" << endl;
+    Downstairs_A(h, 1); // 从第h级，开始下第一步
 
     num = 0; // 方案数初始化
-    cout << "[Method_Rewrite]" << endl;
 
-    Downstairs_rewrite(h, 0);
-
+    cout << "[Method_B]" << endl;
+    Downstairs_B(TARGET_H, 0); // 第0步，从高度 TARGET_H 出发
 
     cout << "总方案数：" << num << endl;
     return 0;
