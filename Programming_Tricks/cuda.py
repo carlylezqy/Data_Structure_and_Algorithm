650141fb8fbda70b9d2e3be3685bf814b88aa75f
CauJoeng <carlylezqy@qq.com>
2022-01-28 18:34:50 +0900

    Update

--- Programming_Tricks/cuda.py
+++ Programming_Tricks/cuda.py
@@ -1,8 +1,4 @@
-import argparse
 import torch
-parser = argparse.ArgumentParser(description='PyTorch Example')
-parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
-args = parser.parse_args()
-args.cuda = not args.disable_cuda and torch.cuda.is_available()
-print(args.disable_cuda)
+cuda = torch.cuda.is_available()
+print(cuda)
 
